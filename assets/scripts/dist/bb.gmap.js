f4fa258143a26ceb62a132200b56ab5cf8c73388
Bene <roch.bene@gmail.com>
2020-05-21 15:01:59 -0400

    Built assets

--- assets/scripts/dist/bb.gmap.js
+++ assets/scripts/dist/bb.gmap.js
@@ -2826,6 +2826,9 @@ BB.gmap.line.prototype.parse_options = function (options) {
     if (typeof options.styles === 'undefined') {
         options.styles = this.controller().default_styles();
     }
+
+    var styles = options.styles;
+    options = this.extend(options, styles);
     return options;
 };
 
